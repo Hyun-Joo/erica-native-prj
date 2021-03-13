e3e610d7cd5df09867a91748df3c5f06b51f5543
Hyun-Joo <dlguswn714@gmail.com>
2021-03-13 23:31:41 +0900

    스타일 적용

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.yellowView} />
+      <View style={styles.purpleView}>
+        <Text style={styles.textStyle}>사랑의 배신자여</Text>
+      </View>
     </View>
   );
 }
@@ -15,7 +17,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  yellowView: {
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+  purpleView: {
+    flex: 3,
+    backgroundColor: '#8533ff'
+  },
+  textStyle: {
+    color: 'white',
+    fontSize: 17
+  }
 });
